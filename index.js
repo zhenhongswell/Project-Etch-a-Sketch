438ee89563e21aaab5033a812aa41bfe0edd3089
zhenhongswell <zhenhongswell@gmail.com>
2023-11-12 15:36:58 +0800

    Create 16x16 grid of square divs add createRowDivInContainer func to create div for each col add createDivInRow func to create the grid square for each row use flex box in each row divs

---
+++ index.js
@@ -0,0 +1,26 @@
+// Create a webpage with a 16x16 grid of square divs
+const container = document.querySelector('.container');
+
+function createRowDivInContainer(number){
+    // create divs for each column
+    for (let index = 0; index < number; index++) {
+        const divElement = document.createElement('div');
+        divElement.classList.add('row');
+        container.appendChild(divElement);
+    }
+}
+
+function createDivsInRow(number){
+    // create divs for each rows
+    const rowNodeList = document.querySelectorAll('.row');
+    rowNodeList.forEach(element => {
+        for (let index = 0; index < number; index++) {
+            const divElement = document.createElement('div');
+            divElement.classList.add('grid-square');
+            element.appendChild(divElement);
+        }
+    });
+}
+
+createRowDivInContainer(16);
+createDivInRow(16);
