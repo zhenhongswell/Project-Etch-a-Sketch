a4466e5681afbd2fd3e04c558386a7e9cd2617be
zhenhongswell <zhenhongswell@gmail.com>
2023-11-13 22:05:23 +0800

    add gridSquareAddlistener func to do hover effect add CreatePad func and Button to create a new pad input number. use while loop to remove all squares from container.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // Create a webpage with a 16x16 grid of square divs
 const container = document.querySelector('.container');
-
+const classDefaultBackgroundColor = 'default-bg-color';
+const classMouseoverBackgroundColor = 'mouseover-bg-color';
+let all_div;
 function createRowDivInContainer(number){
     // create divs for each column
     for (let index = 0; index < number; index++) {
         const divElement = document.createElement('div');
         divElement.classList.add('row');
+        // divElement.classList.add('background-color-div');
         container.appendChild(divElement);
     }
 }
@@ -17,10 +20,33 @@ function createDivsInRow(number){
         for (let index = 0; index < number; index++) {
             const divElement = document.createElement('div');
             divElement.classList.add('grid-square');
+            divElement.classList.add(classDefaultBackgroundColor);
             element.appendChild(divElement);
+            gridSquareAddlistener(divElement);
         }
     });
 }
 
-createRowDivInContainer(16);
-createDivsInRow(16);
+function gridSquareAddlistener(element){
+    // Set up a “hover” effect so that the grid divs change color when your mouse passes over them,
+    // leaving a (pixelated) trail through your grid like a pen would.
+    element.addEventListener("mouseenter",()=>{
+        element.classList.remove(classDefaultBackgroundColor);
+        element.classList.add(classMouseoverBackgroundColor);
+    })
+}
+
+function CreatePad(size){
+    // create size x size Pad
+    createRowDivInContainer(size);
+    createDivsInRow(size);
+}
+
+const createPadButton = document.querySelector('.reset-button');
+createPadButton.addEventListener("click",()=>{
+    const aNumber = Number(window.prompt("please enter a number", ""));
+    while (container.hasChildNodes()){
+        container.removeChild(container.firstChild);
+    }
+    CreatePad(aNumber);
+})
